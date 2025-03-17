e2946088ee749d1bf234de0c1b56c6b8af245b79
unknown <valdezgabriel47@gmail.com>
2025-03-17 14:21:10 -0300

    Nuevas funcionalidades

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-document.getElementById('link-proyectos').addEventListener('click', (e)=>{
+document.getElementById('link-proyectos').addEventListener('click', (e) => {
     e.preventDefault()
     const proyectos = document.querySelector('.proyectos')
-    proyectos.scrollIntoView({behavior:'smooth'})
+    proyectos.scrollIntoView({ behavior: 'smooth' })
 });
 
 
+document.querySelectorAll('.titulo-curso-name').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        let contenedor = btn.closest('.titulo-curso');
+        let info = contenedor.querySelectorAll('.info')
+        info.forEach(i => {
+            i.classList.toggle('info-mostrar');
+        })
+    })
+})
 
-window.addEventListener('scroll',()=>{
+
+
+window.addEventListener('scroll', () => {
     let elementos = document.querySelectorAll('.contenido-fade');
 
-    elementos.forEach((elemento)=>{
+    elementos.forEach((elemento) => {
         let posicionElemento = elemento.getBoundingClientRect();
 
-        if (posicionElemento.top < window.innerHeight && posicionElemento.bottom > 0){
+        if (posicionElemento.top < window.innerHeight && posicionElemento.bottom > 0) {
             elemento.classList.add('visible')
         }
-        else{
+        else {
             elemento.classList.remove('visible')
         }
 
